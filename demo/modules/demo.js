20dcbefb6606be62f7c6a1fd87ad60369c2e4436
ghoullier <gregory.houllier@gmail.com>
2012-12-11 15:54:52 +0100

    Update core.js and demo module

--- demo/modules/demo.js
+++ demo/modules/demo.js
@@ -1,26 +1,31 @@
 (function(/*Window*/w, undefined) {
-  // Define sandboxed global variables
-  var d = w.document,
-      c = w.console,
-      Core = w.Core;
-  // Modules namespace
-  var m = Core.modules || {};
-  m.Demo = (function() {
-    var Class = function(/*DOMStringMap*/properties, /*Node*/root) {
-      if (this instanceof Class) {
-        // Specific constructor
-        this.name = properties.name ? properties.name : "default";
-      } else {
-        throw new Error("Illegal constructor");
-      }
-    };
-    Class.prototype.sayMyName = function() {
-      var name = this.$.name.value;
-      if (typeof name === "string" && name.length > 0) {
-        this.name = name;
-        w.alert("Your name is : " + name);
-      }
-    };
-    return Class;
-  }());
+	// Define sandboxed global variables
+	var d = w.document,
+			c = w.console,
+			Core = w.Core;
+	// Modules namespace
+	var m = Core.modules || {};
+	m.Demo = (function() {
+		var Class = function(/*DOMStringMap*/properties, /*Node*/root) {
+			if (this instanceof Class) {
+				// Specific constructor
+				this.name = properties.name ? properties.name : "default";
+			} else {
+				throw new Error("Illegal constructor");
+			}
+		};
+		Core.mixin(Class.prototype, {
+			postCreate: function() {
+				w.alert("Module has been initialized by Core.js");
+			},
+			sayMyName: function() {
+				var name = this.$.name.value;
+				if (typeof name === "string" && name.length > 0) {
+					this.name = name;
+					w.alert("Your name is : " + name);
+				}
+			}
+		});
+		return Class;
+	}());
 }(this));

290616e0d9c52fce2ac7b00588bc569a19346081
Gregory HOULLIER <gregory.houllier@niji.fr>
2013-12-11 18:42:26 +0100

    Ajout du gestionnaire du classes

---
+++ class.js
@@ -0,0 +1,20 @@
+;(function(root, doc, module) {
+  'use strict';
+  function extend(object) {
+    var self = Object.create(this)
+    if (!object) return self
+    Object.keys(object).forEach(function(key) {
+      self[key] = object[key]
+    })
+    return self
+  }
+  function create() {
+    var self = Object.create(this)
+    if (typeof self.constructor === 'function') {
+      self.constructor.apply(self, arguments)
+    }
+    return self
+  }
+  module.extend = extend
+  module.create = create
+}(this, this.document, this.core))

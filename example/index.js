9fc69a3f948885e80530ac1acbaf882a12fcf944
Gregory HOULLIER <gregory.houllier@niji.fr>
2013-12-14 13:20:39 +0100

    Mise à jour de la documentation

--- example/index.js
+++ example/index.js
@@ -1,7 +1,20 @@
 ;(function(root, doc, core) {
   core.controllers.add('Main', core.BaseController.extend({
-    action: function() {
-      console.log('action')
+    initialize: function() {
+      this.$input = this.$('input')
+      this.$list = this.$('ol')
+    },
+    add: function() {
+      var value = this.$input.value
+        , child = doc.createElement('li')
+      child.textContent = value
+      this.$list.appendChild(child)
+    },
+    clear: function() {
+      var $list = this.$list
+      while ($list.firstChild) {
+        $list.removeChild($list.firstChild)
+      }
     }
   }))
 }(this, this.document, this.core))

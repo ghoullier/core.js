54dedff1fa0dd0726ff38123b90e0c0eb9c2d4b5
Gregory HOULLIER <gregory.houllier@niji.fr>
2013-12-14 12:51:22 +0100

    Modification de la hiérarchie d'héritage de l'objet global core

--- src/base.js
+++ src/base.js
@@ -1,11 +1,20 @@
-(function(root, doc, module) {
+(function(root, doc) {
   'use strict';
+  // Define core object
+  var core = Object.create(null)
+  /**
+   * Bind function to a specific contex
+   * @param  {Function} fn      Function to bin
+   * @param  {Object}   context Execution context
+   * @return {Function}         Bounded function
+   */
   function bind(fn, context) {
     return function bound() {
       fn.call(context, arguments)
     }
   }
-  module.bind = bind
-  // Set module has global object
-  root.core = module
-}(this, this.document, this.core = {}))
+  // Public API
+  core.bind = bind
+  // Set core has global object
+  root.core = core
+}(this, this.document))

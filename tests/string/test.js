b226c88e33038fd064553acbddf90678a9fd16ec
Gregory HOULLIER <gregory.houllier@niji.fr>
2013-12-26 11:07:53 +0100

    Mise à jour des TU sur la manipulation des chaines des caractères

--- tests/string/test.js
+++ tests/string/test.js
@@ -1,8 +1,18 @@
 describe('core#string', function() {
-  var BaseController = core.BaseController
   describe('#startsWith', function() {
     it('String should have a method startsWith', function() {
       assert('function' === typeof String.prototype.startsWith, 'Missing startsWith function in String.prototype')
+    })
+  })
+  describe('#firstUpperCase', function() {
+    it('String transform the first letter of a string', function() {
+      assert('Function' === core.string.firstUpperCase('function'), 'First char is not modified')
+    })
+    it('String transform work an empty string', function() {
+      assert('' === core.string.firstUpperCase(''), 'First char is not modified')
+    })
+    it('String transform work a null value', function() {
+      assert(null === core.string.firstUpperCase(null), 'First char is not modified')
     })
   })
 })

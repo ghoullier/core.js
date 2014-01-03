408cd2950c691e7d0e3d2641004121c443286695
Gregory HOULLIER <gregory.houllier@niji.fr>
2014-01-03 16:32:32 +0100

    Suppression des points virgules inuties

--- core/controller.js
+++ core/controller.js
@@ -33,7 +33,7 @@ define([
         types
           // Filter duplicates entries
           .filter(function(item, index, self) {
-            return self.indexOf(item) === index;
+            return self.indexOf(item) === index
           })
           // Add event delegation
           .forEach(function(type) {
